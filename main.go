7c7720c0a08293436012ef8365eeb42d6dc31132
gabferr <gabrielferrandin@hotmail.com>
2023-12-04 19:46:05 -0300

    feat: logger struct

--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-import "github.com/gabferr/minhas_financas/router"
+import (
+	"fmt"
+
+	"github.com/gabferr/minhas_financas/handler/config"
+	"github.com/gabferr/minhas_financas/router"
+)
 
 func main() {
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	//initialize router
 	router.Initialize()
 }
