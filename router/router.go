9da47e39b780fc789cb2d710db20744b73ccd889
gabferr <gabrielferrandin@hotmail.com>
2023-12-04 14:30:33 -0300

    feat: group routes

--- router/router.go
+++ router/router.go
@@ -3,11 +3,11 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
+	//Initialize Router
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run("8080") // listen and serve on 0.0.0.0:8080
+
+	initializeRoutes(router)
+	//Run Server
+	router.Run() // listen and serve on 0.0.0.0:8080
+
 }
