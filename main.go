c53cfa90d4e200f91f677516073aa8373b465a40
gabferr <gabrielferrandin@hotmail.com>
2023-12-06 14:25:48 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/gabferr/minhas_financas/handler/config"
+	"github.com/gabferr/minhas_financas/config"
 	"github.com/gabferr/minhas_financas/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
 	//Initialize Configs
 	err := config.Init()
 	if err != nil {
-		fmt.Println(err)
+		logger.Errorf("config initialization error: %v", err)
 		return
 	}
 	//initialize router
